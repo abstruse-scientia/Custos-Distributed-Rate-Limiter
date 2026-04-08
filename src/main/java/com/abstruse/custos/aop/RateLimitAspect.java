eb1cb41bf0d52411248ea7617974e8718349c06d
abstruse-scientia <priyanshu0496@gmail.com>
2026-04-08 20:37:15 +0530

    feat: Add Request Context build up

--- src/main/java/com/abstruse/custos/aop/RateLimitAspect.java
+++ src/main/java/com/abstruse/custos/aop/RateLimitAspect.java
@@ -3,11 +3,18 @@ package com.abstruse.custos.aop;
 import com.abstruse.custos.annotations.RateLimit;
 import com.abstruse.custos.core.engine.RateLimiterEngine;
 import com.abstruse.custos.core.model.RequestContext;
+import com.abstruse.custos.utility.CustosIPResolver;
+import com.abstruse.custos.utility.CustosUserResolver;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
 @Aspect
 @Component
@@ -15,13 +22,19 @@ import org.springframework.stereotype.Component;
 public class RateLimitAspect {
 
     private final RateLimiterEngine engine;
-
+    private final CustosUserResolver userResolver;
+    private final CustosIPResolver ipResolver;
 
 
     @Around("@annotation(rateLimit)")
     public Object enforceRateLimit(ProceedingJoinPoint proceedingJoinPoint, RateLimit rateLimit) throws Throwable {
 
-        RequestContext requestContext = new RequestContext("test-key");
+        ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        if (attributes == null) {
+            return "unknown";
+        }
+        RequestContext requestContext = getRequestContext(attributes);
+
 
         boolean allowed = engine.allow(requestContext, rateLimit);
 
@@ -29,6 +42,15 @@ public class RateLimitAspect {
             throw new RuntimeException("Rate limited exceeded for");
         }
         return proceedingJoinPoint.proceed();
+    }
+
+    private RequestContext getRequestContext(ServletRequestAttributes attributes) {
+        HttpServletRequest request = attributes.getRequest();
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String userId = userResolver.resolverUserId(request, authentication);
+        String userIP = ipResolver.resolveClientIP(request);
+        return new  RequestContext(userId, userIP);
+
     }
 
 
--- src/main/java/com/abstruse/custos/core/autoconfiguration/CustosAutoConfiguration.java
+++ src/main/java/com/abstruse/custos/core/autoconfiguration/CustosAutoConfiguration.java
@@ -12,6 +12,10 @@ import com.abstruse.custos.resolver.IPKeyResolver;
 import com.abstruse.custos.resolver.KeyResolver;
 import com.abstruse.custos.resolver.KeyResolverFactory;
 import com.abstruse.custos.resolver.UserKeyResolver;
+import com.abstruse.custos.utility.CustosUserResolver;
+import com.abstruse.custos.utility.CustosIPResolver;
+import com.abstruse.custos.utility.DefaultCustosIPResolver;
+import com.abstruse.custos.utility.DefaultCustosUserResolver;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
@@ -27,8 +31,37 @@ import java.util.List;
 
 public class CustosAutoConfiguration {
 
+        // ---------- USER RESOLVER ----
 
-        // ---------- RESOLVERS ----------
+
+        /**
+         * Runs only if user does not provide their own resolver.
+         * Provides defaults for common deployment scenarios
+         * - Spring Security Context holder holding User
+         * - Reading header request
+         */
+        @Bean
+        @ConditionalOnMissingBean(CustosUserResolver.class)
+        public CustosUserResolver  custosUserResolver() {
+            return new DefaultCustosUserResolver();
+        }
+
+        // ---------- IP RESOLVER ----
+
+        /**
+         * Runs only if user does not provide their own resolver.
+         * Provides sensible defaults for common deployment scenarios:
+         * - Proxy/Load Balancer: X-Forwarded-For
+         * - Nginx: X-Real-IP
+         * - Direct Connection: request.getRemoteAddr()
+         */
+        @Bean
+        @ConditionalOnMissingBean(CustosIPResolver.class)
+        public CustosIPResolver custosIPResolver() {
+            return new DefaultCustosIPResolver();
+        }
+
+        // ---------- KEY RESOLVERS ----------
 
         @Bean
         @ConditionalOnMissingBean
