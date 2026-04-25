7680fde0a25aa7c3c7f0c3f336173283b5fc31b2
abstruse-scientia <125831066+abstruse-scientia@users.noreply.github.com>
2026-04-26 01:40:56 +0530

    fix: move scripts to resources

---
+++ src/main/resources/scripts/LUA_SCRIPT_TOKEN_BUCKET.lua
@@ -0,0 +1,34 @@
+local key = KEYS[1]
+
+local capacity = tonumber(ARGV[1])
+local refillRate = tonumber(ARGV[2])
+local now = tonumber(ARGV[3])
+
+local data = redis.call("HMGET", key, "tokens", "lastRefillTime")
+
+local token = tonumber(data[1])
+local lastRefillTime = tonumber(data[2])
+
+if token == nil then
+    token = capacity
+    lastRefillTime = now
+else
+    local elapsedTime = math.max(0, now - lastRefillTime)
+    local refillTokens = (elapsedTime * refillRate) / 1000
+    token = math.min(capacity, token + refillTokens)
+
+end
+local allowed = 0
+local retryAfterSeconds
+if token >= 1 then
+    token = token - 1
+    allowed = 1
+else
+    retryAfterSeconds = math.max(1, math.ceil(1 / lastRefillTime))
+end
+
+redis.call("HMSET", key, "tokens", token  , "lastRefillTime", now)
+
+redis.call("PEXPIRE", key, 60000)
+
+return { allowed, retryAfterSeconds }
